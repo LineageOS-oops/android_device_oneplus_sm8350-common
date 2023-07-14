f6a5a3a2027af2f64798632d87e3a2c167d19fb9
Vishalcj17 <vishalcj@aospa.co>
2023-07-14 17:11:06 +0300

    sm8350-common: Disable telephony debug logging on production build.

--- common.mk
+++ common.mk
@@ -396,6 +396,14 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ims.disableADBLogs=1 \
+    persist.vendor.ims.disableDebugLogs=1 \
+    persist.vendor.ims.disableIMSLogs=1 \
+    persist.vendor.ims.disableQXDMLogs=1
+endif
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
