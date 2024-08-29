f0329cb5466f3b10605a9996fe5033e25fce3ac2
pjgowtham <pjgowtham@gmail.com>
2024-08-29 21:21:34 +0530

    sm8350-common: Patch urcc ndk backend to load v30 libjsoncpp
    E [UAH_CLIENT_LIB]: libUah load fail, dlopen failed:
    cannot locate symbol "_ZN4Json5ValueaSES0_" referenced by
    "/odm/lib64/vendor.oplus.hardware.urcc-V1-ndk_platform.so"...
    
    Change-Id: I97ddd8cd239b2f8f2a20a6630ddd5e9d2d7dd2ae

--- extract-files.sh
+++ extract-files.sh
@@ -140,6 +140,9 @@ function blob_fixup() {
         odm/lib/libdlbdsservice_v3_6.so | odm/lib/libstagefright_soft_ddpdec.so | odm/lib/libstagefright_soft_ac4dec.so | odm/lib/libstagefrightdolby.so | odm/lib64/libdlbdsservice_v3_6.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        odm/lib64/vendor.oplus.hardware.urcc-V1-ndk_platform.so)
+            grep -q libjsoncpp-v30.so "${2}" || "${PATCHELF}" --replace-needed "libjsoncpp.so" "libjsoncpp-v30.so" "${2}"
+            ;;
         *)
             return 1
             ;;
